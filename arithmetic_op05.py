8c0e7bd3389f8e2c6f6c701a2357a0a9171cad81
Muxtor <jalilovmuxtor203@gmail.com>
2022-11-12 13:45:52 +0500

    arithmetic_op05

--- arithmetic_op05.py
+++ arithmetic_op05.py
@@ -1,5 +1,6 @@
 #Create a variable called 'number' with data type int.
-
+a = 34
 #Create a variable called 'answer' and assign the remainder of the division of number by 3 to it.
-
+b = 34%10*10+34//10
 #Print 'answer'.
+print(b)
